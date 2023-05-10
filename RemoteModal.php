369e1267861871f2fbb26d8cb8503ed43b09b988
Stefano Mtangoo <stefano@hosannahighertech.co.tz>
2023-05-10 09:19:25 +0300

    Update to depend on Bootstrap 5

--- RemoteModal.php
+++ RemoteModal.php
@@ -5,7 +5,7 @@ namespace cenotia\components\modal;
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 
-class RemoteModal extends \yii\bootstrap\Modal
+class RemoteModal extends \yii\bootstrap5\Modal
 {
     const TYPE_DEFAULT = 'default';
 
@@ -14,8 +14,8 @@ class RemoteModal extends \yii\bootstrap\Modal
 
     public function run()
     {
-	parent::run();
-	$this->registerAssets();
+        parent::run();
+        $this->registerAssets();
     }
 
     /**
@@ -25,13 +25,13 @@ class RemoteModal extends \yii\bootstrap\Modal
     {
         $view = $this->getView();
         RemoteModalAsset::register($view);
- 	$uuid = uniqid();
+        $uuid = uniqid();
 
-    	$js = "rm_$uuid = new RemoteModal('#$this->id');\n";
-    	$js .= "$(document).on('click', '[role=\"$this->id\"]', function (event){ \n ";
+        $js = "rm_$uuid = new RemoteModal('#$this->id');\n";
+        $js .= "$(document).on('click', '[role=\"$this->id\"]', function (event){ \n ";
         $js .= "event.preventDefault(); \n";
-        $js.= "rm_$uuid.open(this, null);\n";
-    	$js .="});\n";
-	$view->registerJs($js,\yii\web\View::POS_READY);
+        $js .= "rm_$uuid.open(this, null);\n";
+        $js .= "});\n";
+        $view->registerJs($js, \yii\web\View::POS_READY);
     }
 }
--- RemoteModalAsset.php
+++ RemoteModalAsset.php
@@ -7,22 +7,20 @@ use yii\web\AssetBundle;
 
 class RemoteModalAsset extends AssetBundle
 {
-     public $sourcePath = '@vendor/cenotia/yii2-remote-modal/assets';
+    public $sourcePath = '@vendor/cenotia/yii2-remote-modal/assets';
 
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
+        'yii\bootstrap5\BootstrapAsset',
+        'yii\bootstrap5\BootstrapPluginAsset',
     ];
-   
-   public $css = [
-	'css/remote-modal.css'
-   ];
-    
+
+    public $css = [
+        'css/remote-modal.css'
+    ];
+
     public $js = [
-    'js/hasAttr.js',    
-	'js/remote-modal.js'
+        'js/hasAttr.js',
+        'js/remote-modal.js'
     ];
-
-
 }
